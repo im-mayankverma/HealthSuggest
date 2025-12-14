3a5e1d516b5077be8a0123d2e327cbf8c0673be5
Mayank <mkverma7287@gmail.com>
2025-12-14 13:45:31 +0530

    Change database from MySQL to SQLite

--- healthsuggest/settings.py
+++ healthsuggest/settings.py
@@ -73,18 +73,12 @@ WSGI_APPLICATION = 'healthsuggest.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'healthsuggest',
-        'USER': 'root',
-        'PASSWORD': '2004',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
